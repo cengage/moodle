33c3bcaa8975285cd2f3c7d5a0b7c84682a57099
Claude Vervoort <claude.vervoort@cengage.com>
2020-07-20 11:17:31 -0400

    MDL-67301 LTI: wip on openidconf

--- mod/lti/openid-configuration.php
+++ mod/lti/openid-configuration.php
@@ -25,30 +25,31 @@
 define('NO_DEBUG_DISPLAY', true);
 define('NO_MOODLE_COOKIES', true);
 
+global $CFG;
 require_once(__DIR__ . '/../../config.php');
+require_once($CFG->libdir.'/weblib.php');
 
 $conf = [
-    "issuer" => "https://server.example.com",
-    "authorization_endpoint" =>  "https://server.example.com/connect/authorize",
-    "token_endpoint" => "https://server.example.com/connect/token",
-    "token_ endpoint_auth_methods_supported" => ["private_key_jwt"],
-    "token_endpoint_auth_signing_alg_values_supported" => ["RS256"],
-    "jwks_uri" => "https://server.example.com/jwks.json",
-    "registration_endpoint" => "https://server.example.com/connect/register",
-    "scopes_supported" => ["openid",
-        "https://purl.imsglobal.org/spec/lti-gs/scope/contextgroup.readonly",
-        "https://purl.imsglobal.org/spec/lti-ags/scope/lineitem",
-        "https://purl.imsglobal.org/spec/lti-ags/scope/result.readonly",
-        "https://purl.imsglobal.org/spec/lti-ags/scope/score",
-        "https://purl.imsglobal.org/spec/lti-reg/scope/registration"],
-    "response_types_supported" => ["id_token"],
-    "subject_types_supported" => ["public", "pairwise"],
-    "id_token_signing_alg_values_supported" => ["RS256"],
-    "claims_supported" => ["sub", "iss", "name", "given_name", "family_name", "email"],
-    "https://purl.imsglobal.org/spec/lti-platform-configuration " => [
-        "messages_supported" => ["LtiResourceLink", "LtiDeepLinkingRequest", "LtiDeploymentRequest"],
-        "placements" => ["AddContentMenu", "AddToRichTextMenu", "CourseNavigation"],
-        "variables" => ["CourseSection.timeFrame.end", "CourseSection.timeFrame.begin", "Context.id.history", "ResourceLink.id.history"]
+    'issuer' => $CFG->wwwroot,
+    'token_endpoint' => (new moodle_url('/mod/lti/token.php'))->out(false),
+    'token_endpoint_auth_methods_supported' => ['private_key_jwt'],
+    'token_endpoint_auth_signing_alg_values_supported' => ['RS256'],
+    'jwks_uri' => (new moodle_url('/mod/lti/certs.php'))->out(false),
+    'registration_endpoint' => (new moodle_url('/mod/lti/startltiadvregistration.php'))->out(false),
+    'scopes_supported' => ['openid',
+        'https://purl.imsglobal.org/spec/lti-gs/scope/contextgroup.readonly',
+        'https://purl.imsglobal.org/spec/lti-ags/scope/lineitem',
+        'https://purl.imsglobal.org/spec/lti-ags/scope/result.readonly',
+        'https://purl.imsglobal.org/spec/lti-ags/scope/score',
+        'https://purl.imsglobal.org/spec/lti-reg/scope/registration'],
+    'response_types_supported' => ['id_token'],
+    'subject_types_supported' => ['public', 'pairwise'],
+    'id_token_signing_alg_values_supported' => ['RS256'],
+    'claims_supported' => ['sub', 'iss', 'name', 'given_name', 'family_name', 'email'],
+    'https://purl.imsglobal.org/spec/lti-platform-configuration ' => [
+        'messages_supported' => ['LtiResourceLink', 'LtiDeepLinkingRequest', 'LtiDeploymentRequest'],
+        'placements' => ['AddContentMenu', 'AddToRichTextMenu', 'CourseNavigation'],
+        'variables' => ['CourseSection.timeFrame.end', 'CourseSection.timeFrame.begin', 'Context.id.history', 'ResourceLink.id.history']
     ]
 ];
 
