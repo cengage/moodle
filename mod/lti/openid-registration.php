660218e29f8711439f46831beb71eb27217a33ac
Claude Vervoort <claude.vervoort@cengage.com>
2020-09-25 17:15:37 -0400

    MDL-67301 no-debug headers

--- mod/lti/openid-registration.php
+++ mod/lti/openid-registration.php
@@ -21,6 +21,8 @@
  * @package    mod_lti
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+define('NO_DEBUG_DISPLAY', true);
+define('NO_MOODLE_COOKIES', true);
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/mod/lti/locallib.php');
