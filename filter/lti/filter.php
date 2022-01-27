f47bf3659311ec7b18ed42e4f955bc9b100b8e7a
Claude Vervoort <claude.vervoort@cengage.com>
2022-01-27 12:56:25 -0500

    MDL-67560 mod_lti: atto, lti filter use context, support popup

--- filter/lti/filter.php
+++ filter/lti/filter.php
@@ -2,8 +2,10 @@
 class filter_lti extends moodle_text_filter {
 
     public function filter($text, array $options = array()) {
-    
-        if (!is_string($text) or empty($text) or stripos($text, 'href="/lib/editor/atto/plugins/lti/launch.php') === false ) {
+   
+        $coursecontext = $this->context->get_course_context(false);
+        // LTI launches for now only execute in course contexts.
+        if ($coursecontext && !is_string($text) or empty($text) or stripos($text, 'data-lti') === false ) {
             return $text;
         }
 
@@ -15,9 +17,36 @@ class filter_lti extends moodle_text_filter {
         $newtext = '';
 
         foreach ($matches as $idx => $val) {
-            if (stripos($val, "<a ") === 0 && preg_match('/href=\"(\/lib\/editor\/atto\/plugins\/lti\/launch\.php.*?)\"/', $val, $hrefmatches) !== false) {
+            //if (stripos($val, "<a ") === 0 && preg_match('/href=\"(\/lib\/editor\/atto\/plugins\/lti\/launch\.php.*?)\"/', $val, $hrefmatches) !== false) {
+            if (stripos($val, "<a ") === 0 
+                && preg_match('/data-lti=\"([^\"]*)/', $val, $ltidatamatches) === 1  
+                && preg_match('/href=\"([^\"]*)/', $val, $hrefmatches) === 1) {
                 // iframe if only embed, window.open otherwise to keep the relationship with opener
-                $newtext.="<iframe src=\"$hrefmatches[1]\"></iframe>";
+                $href = $hrefmatches[1];
+                $sep = str_contains($href, '?')?'&':'?';
+                $href = $href.$sep."courseid=".$coursecontext->instanceid;
+                if (strpos($ltidatamatches[1], 'embed') === 0) {
+                    $width = '90%';
+                    $height = '400px';
+                    if (preg_match('/width:([^;]*)/', $ltidatamatches[1], $widthmatches) === 1) {
+                        $width = $widthmatches[1];
+                    }
+                    if (preg_match('/height:([^;1]*)/', $ltidatamatches[1], $heightmatches) === 1) {
+                        $height = $heightmatches[1];
+                    }
+                    $newtext.="<iframe class=\"ltiembed\" src=\"$href\" style=\"width:$width;height:$height\"></iframe>";
+                } else {
+                    /*$windowname = "ltipopup";
+                    if (preg_match('/windowname:([^;]*)/', $ltidatamatches[1], $namematches) === 1) {
+                        $windowname = $namematchs[1];
+                    }
+                    $elementid=$elprefix.'_'.$idx;
+                    $newtext.=str_replace("<a ", "<a id=\"$elementid\"", $val);
+                    $newtext.="<script>document.getElementById(\"$elementid\").addEventListener(\"click\",";
+                    $newtext.="function open_$elementid(e) {window.open(\"$hrefmatches[1]\",\"$windowname\");e.preventDefault();});</script>";
+                    */
+                    $newtext.=str_replace($hrefmatches[1], $href, $val);
+                }
             } else {
                 $newtext.=$val;
             }
---
+++ filter/lti/tests/filter_test.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests.
+ *
+ * @package filter_lti
+ * @category test
+ * @copyright 2021 Cengage Group
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/filter/lti/filter.php');
+
+/**
+ * Tests for filter_lti
+ *
+ * @copyright 2021 Cengage Group
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class filter_lti_testcase extends advanced_testcase {
+
+    /**
+     * Tests the filtering of lti hyperlinks
+     *
+     */
+    function test_filtering_no_lti() {
+        $this->resetAfterTest(true);
+        $course = $this->getDataGenerator()->create_course();
+        $context = context_course::instance($course->id);
+        $filter = new filter_lti($context, array());
+        $noltilinkhere = "bla bla <a href=\"https://nowhere\">test</a> bla bla";
+        $this->assertEquals($noltilinkhere, $filter->filter($noltilinkhere));
+    }
+    
+    function test_filtering_embed_link_transform() {
+        $this->resetAfterTest(true);
+        $course = $this->getDataGenerator()->create_course();
+        $context = context_course::instance($course->id);
+        $filter = new filter_lti($context, array());
+        $withltilink = "bla bla <a data-lti=\"embed\" href=\"https://nowhere?a=1\">test</a> bla bla";
+        $this->assertTrue(str_contains($filter->filter($withltilink), "<iframe class=\"ltiembed\" src=\"https://nowhere?a=1&courseid=$course->id\" style=\"width:90%;height:400px\">"));
+        $this->assertFalse(str_contains($filter->filter($withltilink), "<a "));
+    }
+
+    function test_filtering_embed_link_transform_with_widthheight() {
+        $this->resetAfterTest(true);
+        $course = $this->getDataGenerator()->create_course();
+        $context = context_course::instance($course->id);
+        $filter = new filter_lti($context, array());
+        $withltilink = "bla bla <a data-lti=\"embed;width:300px;height:440px\" href=\"https://nowhere\">test</a> bla bla";
+        $this->assertTrue(str_contains($filter->filter($withltilink), "<iframe class=\"ltiembed\" src=\"https://nowhere?courseid=$course->id\" style=\"width:300px;height:440px\">"));
+        $this->assertFalse(str_contains($filter->filter($withltilink), "<a "));
+    }
+    
+    function test_filtering_notenmbed_addcourseid() {
+        $this->resetAfterTest(true);
+        $course = $this->getDataGenerator()->create_course();
+        $context = context_course::instance($course->id);
+        $filter = new filter_lti($context, array());
+        $withltilink = "bla bla <a data-lti=\"newwin\" href=\"https://nowhere\">test</a> bla bla";
+        $this->assertEquals("bla bla <a data-lti=\"newwin\" href=\"https://nowhere?courseid=$course->id\">test</a> bla bla", $filter->filter($withltilink));
+    }
+}
