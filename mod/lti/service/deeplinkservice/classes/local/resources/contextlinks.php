1fc5e590013054fc8d15735d27c517b2a4df8b0b
Claude Vervoort <claude.vervoort@cengage.com>
2022-11-04 14:09:07 -0400

    dls: initial commit

---
+++ mod/lti/service/deeplinkservice/classes/local/resources/contextlinks.php
@@ -0,0 +1,123 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains a class definition for the Context Memberships resource
+ *
+ * @package    ltiservice_memberships
+ * @copyright  2015 Vital Source Technologies http://vitalsource.com
+ * @author     Stephen Vickers
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+namespace ltiservice_deeplinkservice\local\resources;
+
+use mod_lti\local\ltiservice\resource_base;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * A resource implementing Context Memberships.
+ *
+ * @package    ltiservice_memberships
+ * @since      Moodle 3.0
+ * @copyright  2015 Vital Source Technologies http://vitalsource.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class contextlinks extends resource_base {
+
+ /**
+     * Class constructor.
+     *
+     * @param \ltiservice_memberships\local\service\memberships $service Service instance
+     */
+    public function __construct($service) {
+
+        parent::__construct($service);
+        $this->id = 'DeepLinkContextResource';
+        $this->template = '/{context_id}/bindings/{tool_code}/contextlinks';
+        //$this->variables[] = 'ToolProxyBinding.memberships.url';
+        $this->formats[] = 'application/vnd.1edtech.lti.deeplinkingitems+json';
+        $this->methods[] = self::HTTP_GET;
+    }
+
+    /**
+     * Execute the request for this resource.
+     *
+     * @param \mod_lti\local\ltiservice\response $response  Response object for this request.
+     */
+    public function execute($response) {
+        global $DB;
+
+        $params = $this->parse_template();
+        $limitnum = optional_param('limit', 0, PARAM_INT);
+        $limitfrom = optional_param('from', 0, PARAM_INT);
+
+        if ($limitnum <= 0) {
+            $limitfrom = 0;
+        }
+
+        try {
+            /*
+            if (!$this->check_tool($params['tool_code'], $response->get_request_data(),
+                array(memberships::SCOPE_MEMBERSHIPS_READ))) {
+                throw new \Exception(null, 401);
+            }
+            */
+            if (!($course = $DB->get_record('course', array('id' => $params['context_id']), 'id,shortname,fullname',
+                IGNORE_MISSING))) {
+                throw new \Exception("Not Found: Course {$params['context_id']} doesn't exist", 404);
+            }
+            if (!$this->get_service()->is_allowed_in_context($params['tool_code'], $course->id)) {
+                throw new \Exception(null, 404);
+            }
+            if (!($context = \context_course::instance($course->id))) {
+                throw new \Exception("Not Found: Course instance {$course->id} doesn't exist", 404);
+            }
+
+            $links = $this->get_service()->get_links($this, $context, $course, $params['tool_code'], $limitfrom, $limitnum, $response);
+
+            $response->set_body(json_encode(['items' => $links]));
+
+        } catch (\Exception $e) {
+            $response->set_code($e->getCode());
+            $response->set_reason($e->getMessage());
+        }
+    }
+
+    /**
+     * Parse a value for custom parameter substitution variables.
+     *
+     * @param string $value String to be parsed
+     *
+     * @return string
+     */
+    public function parse_value($value) {
+        global $COURSE, $DB;
+
+        if (strpos($value, '$DeepLinkService.contextUrl') !== false) {
+            $this->params['context_id'] = $COURSE->id;
+            if ($tool = $this->get_service()->get_type()) {
+                $this->params['tool_code'] = $tool->id;
+            }
+            $value = str_replace('$DeepLinkService.contextUrl', parent::get_endpoint(), $value);
+        }
+        return $value;
+
+    }
+
+}
---
+++ mod/lti/service/deeplinkservice/classes/local/service/deeplinkservice.php
@@ -0,0 +1,187 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains a class definition for the LTI Gradebook Services
+ *
+ * @package    ltiservice_gradebookservices
+ * @copyright  2017 Cengage Learning http://www.cengage.com
+ * @author     Dirk Singels, Diego del Blanco, Claude Vervoort
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace ltiservice_deeplinkservice\local\service;
+
+use mod_lti\local\ltiservice\service_base;
+use moodle_url;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/mod/lti/locallib.php');
+
+/**
+ * A service implementing LTI Gradebook Services.
+ *
+ * @package    ltiservice_gradebookservices
+ * @copyright  2017 Cengage Learning http://www.cengage.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class deeplinkservice extends service_base {
+
+    /** Scope for reading membership data */
+    const SCOPE_DEEPLINKING_READ = 'https://purl.imsglobal.org/spec/lti/scope/deeplinkingitem.readonly';
+    const SCOPE_DEEPLINKING_UPDATE = 'https://purl.imsglobal.org/spec/lti/scope/deeplinkingitem.update';
+    const SCOPE_DEEPLINKING_ALL = 'https://purl.imsglobal.org/spec/lti/scope/deeplinkingitem';
+
+    /**
+     * Class constructor.
+     */
+    public function __construct() {
+
+        parent::__construct();
+        $this->id = 'deeplinkservice';
+        $this->name = get_string($this->get_component_id(), $this->get_component_id());
+
+    }
+
+    /**
+     * Get the resources for this service.
+     *
+     * @return array
+     */
+    public function get_resources() {
+
+        if (empty($this->resources)) {
+            $this->resources = array();
+            $this->resources[] = new \ltiservice_deeplinkservice\local\resources\contextlinks($this);
+        }
+
+        return $this->resources;
+
+    }
+
+    /**
+     * Get the scope(s) permitted for the tool relevant to this service.
+     *
+     * @return array
+     */
+    public function get_permitted_scopes() {
+
+        $scopes = array();
+        $ok = !empty($this->get_type());
+        if ($ok) {
+            $scopes[] = self::SCOPE_DEEPLINKING_READ;
+            $scopes[] = self::SCOPE_DEEPLINKING_UPDATE;
+            $scopes[] = self::SCOPE_DEEPLINKING_ALL;
+        }
+        /*
+        if ($ok && isset($this->get_typeconfig()[$this->get_component_id()]) &&
+            ($this->get_typeconfig()[$this->get_component_id()] == parent::SERVICE_ENABLED)) {
+            $scopes[] = self::SCOPE_MEMBERSHIPS_READ;
+        }
+        */
+        return $scopes;
+
+    }
+
+    /**
+     * Get the scope(s) defined by this service.
+     *
+     * @return array
+     */
+    public function get_scopes() {
+        return [self::SCOPE_DEEPLINKING_ALL, self::SCOPE_DEEPLINKING_READ, self::SCOPE_DEEPLINKING_UPDATE];
+    }
+
+    /**
+     * Get the JSON for existing links.
+     *
+     * @param \mod_lti\local\ltiservice\resource_base $resource       Resource handling the request
+     * @param \context_course   $context    Course context
+     * @param \course           $course     Course
+     * @param string            $role       User role requested (empty if none)
+     * @param int               $limitfrom  Position of first record to be returned
+     * @param int               $limitnum   Maximum number of records to be returned
+     * @param object            $lti        LTI instance record
+     * @param \core_availability\info_module $info Conditional availability information
+     *      for LTI instance (null if context-level request)
+     * @param \mod_lti\local\ltiservice\response $response       Response object for the request
+     *
+     * @return string
+     */
+    public function get_links($resource, $context, $course, $typeid, $limitfrom, $limitnum, $response) {
+        global $DB;
+        //$type = $DB->get_record('lti_types', array('id' => $typeid));
+        //TODO: eventually need to check by URL too for instances without typeid.
+        $links = array_values($DB->get_records('lti', array('course' => $course->id, 'typeid' => $typeid)));
+        $func = function($l) {
+            return $this->toLink($l);
+        };
+        return array_map($func, $links);
+    }
+
+    public function toLink($lti) {
+        return [
+            'title' => $lti->name
+        ];
+    }
+
+
+    /**
+     * Adds form elements for membership add/edit page.
+     *
+     * @param \MoodleQuickForm $mform
+     */
+    public function get_configuration_options(&$mform) {
+        $elementname = $this->get_component_id();
+        $options = [
+            get_string('notallow', $this->get_component_id()),
+            get_string('allow', $this->get_component_id())
+        ];
+
+        $mform->addElement('select', $elementname, get_string($elementname, $this->get_component_id()), $options);
+        $mform->setType($elementname, 'int');
+        $mform->setDefault($elementname, 0);
+        $mform->addHelpButton($elementname, $elementname, $this->get_component_id());
+    }
+
+    /**
+     * Return an array of key/values to add to the launch parameters.
+     *
+     * @param string $messagetype 'basic-lti-launch-request' or 'ContentItemSelectionRequest'.
+     * @param string $courseid The course id.
+     * @param string $user The user id.
+     * @param string $typeid The tool lti type id.
+     * @param string $modlti The id of the lti activity.
+     *
+     * The type is passed to check the configuration
+     * and not return parameters for services not used.
+     *
+     * @return array of key/value pairs to add as launch parameters.
+     */
+    public function get_launch_parameters($messagetype, $courseid, $user, $typeid, $modlti = null) {
+        $launchparameters = [];
+        $tool = lti_get_type_type_config($typeid);
+        if (isset($tool->{$this->get_component_id()})) {
+            if ($tool->{$this->get_component_id()} == parent::SERVICE_ENABLED && $this->is_used_in_context($typeid, $courseid)) {
+                $launchparameters['deeplink_context_url'] = '$DeepLinkService.contextUrl';
+            }
+        }
+        return $launchparameters;
+    }
+
+}
